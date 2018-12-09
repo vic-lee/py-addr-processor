039e22c19985c2ced50aa38c8d66f653f7127423
vic-lee <viclee@nyu.edu>
2018-12-09 09:33:07 -0500

    Creation function definitions

---
+++ cahce_addr_processor.py
@@ -0,0 +1,20 @@
+def get_addr_tag(addr, tag_len):
+    pass
+
+def get_addr_index(addr, index_len, tag_len):
+    pass
+
+def get_addr_offset(addr, offset_len, addr_len):
+    pass
+
+def get_offset_len(block_size):
+    pass
+
+def get_index_len(block_num, set_size):
+    pass
+
+def is_divisible(numerator, denominator):
+    pass
+
+def main:
+    pass

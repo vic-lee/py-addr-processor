6374257067a852c77b9b5579fd391bb45c64f465
vic-lee <viclee@nyu.edu>
2018-12-09 10:11:00 -0500

    Implement hex to bin method

--- cache_addr_processor.py
+++ cache_addr_processor.py
@@ -27,13 +27,20 @@ def get_tag_len(addr_len, index_len, offset_len):
 def is_divisible(numerator, denominator):
     return numerator % denominator == 0
 
+def hex_to_bin(hex_num):
+    dec_int = int(hex_num, 16)
+    bin_int = bin(dec_int)
+    bin_str = str(bin_int)[2:]  # strip out 0b prefix
+    return bin_str
+
 def main():
-    address_bin = "101010111100110111101111"
+    address_hex = "0xABCDEF"
+    address_bin = hex_to_bin(address_hex)
+    print(address_bin)
     address_len = len(address_bin)
     block_num = 4096
     set_size = 16      # n-way associative
     block_size = 16
-
     index_len = get_index_len(block_num, set_size)
     offset_len = get_offset_len(block_size)
     tag_len = get_tag_len(address_len, index_len, offset_len)
@@ -42,7 +49,8 @@ def main():
     address_index = get_addr_index(address_bin, index_len, tag_len)
     address_offset = get_addr_offset(address_bin, offset_len)
 
-    print('Tag is {}\nIndex is {}\nOffset is {}'.format(address_tag, address_index, address_offset))
+    print('Tag is {}\nIndex is {}\nOffset is {}\
+        '.format(address_tag, address_index, address_offset))
 
 if __name__ == '__main__':
     main()

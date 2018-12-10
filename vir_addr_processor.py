8e0892773677a8200c63952f8dc8e679443e297f
vic-lee <viclee@nyu.edu>
2018-12-09 20:51:00 -0500

    Update function comments in vir addr processing

--- vir_addr_processor.py
+++ vir_addr_processor.py
@@ -1,6 +1,26 @@
 import math
 from util import hex_to_bin, bin_to_hex
 
+'''
+About this script:
+
+VA can be partitioned into:
+
+    |------------------------------------|----------------|
+    |         Virtual Page Number        |     Offset     |
+
+Partioning an address requires 2 inputs:
+
+1. the address itself
+2. page Size
+
+We can derive offset size from page size; after having page size and
+the address, we have the size of the VPN. After acquiring the size
+of VPN and offset, we perform substring operations to get VPN and offset.
+
+Author: Vic Lee
+'''
+
 def get_vpn(addr, vpn_len):
     return addr[:vpn_len]
 
@@ -8,6 +28,11 @@ def get_vpo(addr, vpn_len):
     return addr[vpn_len:]
 
 def get_offset_len(page_size):
+    '''
+    The length of offset is determined by how large the page size is.
+    The larger the page is, the more offset digits are required to
+    represent which byte houses the Physical Address is at.
+    '''
     return int(math.log(page_size, 2))
 
 def get_vpn_len(addr, offset_len):
@@ -30,7 +55,7 @@ def main():
 
     addr_vpn_hex = bin_to_hex(addr_vpn)
     addr_vpo_hex = bin_to_hex(addr_vpo)
-    
+
     print("\nThe Virtual Page Number of your address is {}\n\
         ".format(addr_vpn))
     print("The Virtual Page Offset of your address is {}\n\

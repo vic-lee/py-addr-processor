3ab77876f9850bc9a5c3ffea0bf5049c622ead93
vic-lee <viclee@nyu.edu>
2018-12-09 20:44:14 -0500

    Fix logical error in VA offset calculations

--- vir_addr_processor.py
+++ vir_addr_processor.py
@@ -7,26 +7,39 @@ def get_vpn(addr, vpn_len):
 def get_vpo(addr, vpn_len):
     return addr[vpn_len:]
 
-def get_vpn_len(page_size):
+def get_offset_len(page_size):
     return int(math.log(page_size, 2))
 
+def get_vpn_len(addr, offset_len):
+    return len(addr) - offset_len
+
 def get_user_input():
-    address_hex = input("Address in hex:")
-    page_size = int(input("VA page size:"))
+    address_hex = input("Address in hex: ")
+    page_size = int(input("VA page size: "))
     return address_hex, page_size
 
 def main():
     address_hex, page_size = get_user_input()
     address_bin = hex_to_bin(address_hex)
-    vpn_len = get_vpn_len(page_size)
+
+    offset_len = get_offset_len(page_size)
+    vpn_len = get_vpn_len(address_bin, offset_len)
+
     addr_vpn = get_vpn(address_bin, vpn_len)
     addr_vpo = get_vpo(address_bin, vpn_len)
 
     addr_vpn_hex = bin_to_hex(addr_vpn)
     addr_vpo_hex = bin_to_hex(addr_vpo)
-    print("\nThe Virtual Page Number of your address is {}\nThe Virtual Page Offset of your address is {}\n".format(addr_vpn, addr_vpo))
+    
+    print("\nThe Virtual Page Number of your address is {}\n\
+        ".format(addr_vpn))
+    print("The Virtual Page Offset of your address is {}\n\
+        ".format(addr_vpo))
 
-    print("\nThe Virtual Page Number of your address in hex is {}\nThe Virtual Page Offset of your address in hex is {}\n".format(addr_vpn_hex, addr_vpo_hex))
+    print("\nThe Virtual Page Number of your address in hex is {}\n\
+        ".format(addr_vpn_hex))
+    print("The Virtual Page Offset of your address in hex is {}\n\
+        ".format(addr_vpo_hex))
 
 if __name__ == '__main__':
     main()
